70a3e70e96fd0f94204b55dd7394c6274465e922
Simon Steindl <kosimst@gmail.com>
2022-08-03 13:26:36 +0200

    Add window opener data

--- src/components/device-card/data-hooks/window-opener.ts
+++ src/components/device-card/data-hooks/window-opener.ts
@@ -1,3 +1,4 @@
+import { useCallback, useMemo } from 'react'
 import useDeviceState from '../../../hooks/use-device-state'
 import DataHook from '../use-data-hook/data-hook'
 
@@ -8,7 +9,37 @@ const useData: DataHook = (device) => {
     -0.5
   )
 
-  return {}
+  const texts = useMemo(
+    () => [
+      {
+        id: 'opened-state',
+        text:
+          openedLevel === -0.5
+            ? 'Locked'
+            : openedLevel === 100
+            ? 'Opened'
+            : `${openedLevel}% opened`,
+      },
+    ],
+    [openedLevel]
+  )
+
+  const onToggleChange = useCallback(
+    (value: boolean) => {
+      setOpenedLevel(value ? 100 : -0.5)
+    },
+    [setOpenedLevel]
+  )
+
+  const toggleValue = useMemo(() => openedLevel > -0.5, [openedLevel])
+
+  return {
+    texts,
+    sliderValue: openedLevel,
+    toggleValue,
+    onSliderChange: setOpenedLevel,
+    onToggleChange,
+  }
 }
 
 export default useData
