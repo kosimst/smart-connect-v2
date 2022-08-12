8250ced6f81465200656604098793fd9f0484ca0
Simon Steindl <kosimst@gmail.com>
2022-08-12 13:32:02 +0200

    Add more to speed test

--- src/components/device-card/data-hooks/speed-test.ts
+++ src/components/device-card/data-hooks/speed-test.ts
@@ -5,23 +5,41 @@ import DataHook, { DataText } from '../use-data-hook/data-hook'
 const useData: DataHook = (device) => {
   const [download] = useDeviceState(device, 'download-megabits', 0)
   const [upload] = useDeviceState(device, 'upload-megabits', 0)
+  const [running] = useDeviceState(device, 'running', false)
+  const [, setTestButton] = useDeviceState(device, 'test-button', true)
 
   const texts = useMemo<DataText[]>(
-    () => [
-      {
-        id: 'download',
-        text: `${download.toFixed(0)}Mbps`,
-      },
-      {
-        id: 'upload',
-        text: `${upload.toFixed(0)}Mbps`,
-      },
-    ],
-    [download, upload]
+    () =>
+      running
+        ? [
+            {
+              id: 'running',
+              text: 'Running...',
+            },
+          ]
+        : [
+            {
+              id: 'download',
+              text: `${download.toFixed(0)}Mbps`,
+            },
+            {
+              id: 'upload',
+              text: `${upload.toFixed(0)}Mbps`,
+            },
+          ],
+    [download, upload, running]
   )
 
   return {
     texts,
+    toggleValue: true,
+    onToggleChange: () => {
+      if (running) {
+        return
+      }
+
+      setTestButton(true)
+    },
   }
 }
 
