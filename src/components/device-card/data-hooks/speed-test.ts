ca3b60d9bfbc615fc191a331fe189147a4380189
Simon Steindl <kosimst@gmail.com>
2022-09-25 17:08:44 +0200

    Remove action for speed test

--- src/components/device-card/data-hooks/speed-test.ts
+++ src/components/device-card/data-hooks/speed-test.ts
@@ -5,41 +5,23 @@ import DataHook, { DataText } from '../use-data-hook/data-hook'
 const useData: DataHook = (device) => {
   const [download] = useDeviceState(device, 'download-megabits', 0)
   const [upload] = useDeviceState(device, 'upload-megabits', 0)
-  const [running] = useDeviceState(device, 'running', false)
-  const [, setTestButton] = useDeviceState(device, 'test-button', true)
 
   const texts = useMemo<DataText[]>(
-    () =>
-      running
-        ? [
-            {
-              id: 'running',
-              text: 'Running...',
-            },
-          ]
-        : [
-            {
-              id: 'download',
-              text: `${download.toFixed(0)}Mbps`,
-            },
-            {
-              id: 'upload',
-              text: `${upload.toFixed(0)}Mbps`,
-            },
-          ],
-    [download, upload, running]
+    () => [
+      {
+        id: 'download',
+        text: `${download.toFixed(0)}Mbps`,
+      },
+      {
+        id: 'upload',
+        text: `${upload.toFixed(0)}Mbps`,
+      },
+    ],
+    [download, upload]
   )
 
   return {
     texts,
-    toggleValue: true,
-    onToggleChange: () => {
-      if (running) {
-        return
-      }
-
-      setTestButton(true)
-    },
   }
 }
 
