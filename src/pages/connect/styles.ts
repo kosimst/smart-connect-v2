e7b8833f17d9cab574a7858b03e44688bc12fddd
Simon Steindl <kosimst@gmail.com>
2022-09-30 14:18:36 +0200

    Connect page responsive

--- src/pages/connect/styles.ts
+++ src/pages/connect/styles.ts
@@ -5,6 +5,16 @@ export const Container = styled.div`
   flex-direction: column;
   gap: 32px;
   padding-top: 16px;
+  margin: auto;
+  max-width: 500px;
+
+  @media (min-width: 500px) {
+    margin-top: 4vw;
+
+    & > *:first-child {
+      margin-bottom: 1vw;
+    }
+  }
 `
 
 export const FormContainer = styled.form`
