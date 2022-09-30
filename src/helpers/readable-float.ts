acaf4583819c0e091d3a1a756f4c199888175c54
Simon Steindl <kosimst@gmail.com>
2022-09-30 13:52:18 +0200

    Always use dots as comma

--- src/helpers/readable-float.ts
+++ src/helpers/readable-float.ts
@@ -1,6 +1,6 @@
 const readableFloat = (value: number, precision = 2) => {
   const rounded = Math.round(value * 10 ** precision) / 10 ** precision
-  return rounded.toLocaleString(undefined, {
+  return rounded.toLocaleString('en-US', {
     minimumFractionDigits: precision,
     maximumFractionDigits: precision,
   })
